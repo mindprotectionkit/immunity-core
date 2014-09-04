83f606c0f2d9abbc8f4c3988d93d0922c6b0e9b9
Mattias Schlenker <ms@mattiasschlenker.de>
2014-09-04 17:55:21 +0200

    add IP forwarding

--- patches/etc-rc.d-0100-immuvm.sh
+++ patches/etc-rc.d-0100-immuvm.sh
@@ -7,8 +7,12 @@ export PATH
 case $1 in
     start)
 	printf "${bold}Searching and starting VMs... ${normal}\n"
+	[ -d /dev/net ] || mkdir /dev/net
+	[ -c /dev/net/tun ] || mknod /dev/net/tun c 10 200
+	# Enable IP forwarding:
+	echo 1 > /proc/sys/net/ipv4/ip_forward > /dev/null 2>&1 
 	vmparts=''
-	cat /proc/cmdline | while read ctok ; do
+	for ctok in ` cat /proc/cmdline ` ; do
 		case $ctok in
 			vmparts=*) 
 				vmparts=` echo "$ctok" | awk -F '='  '{print $2}' `
@@ -20,8 +24,8 @@ case $1 in
 			device=` blkid | grep $p | awk '{print $1}' | sed 's/\://g' `  
 			if [ -n "$device" ] ; then
 				mkdir -p "/immuvm/$p"
-				mount -o rw,relatime "$device" "/immuvm/$p"
-				find "/immuvm/$p" -type f -name immuvm.sh -exec /bin/ash {} \; 
+				mountpoint -q "/immuvm/$p" || mount -o rw,relatime "$device" "/immuvm/$p"
+				find "/immuvm/$p" -type f -name immuvm.sh -exec /bin/ash {} start \; 
 			fi
 		done
 	fi
